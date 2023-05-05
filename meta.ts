9258b093764e798205ded848181832aa3673fd2d
yamuna B <yamuna.b@inncircles.com>
2023-05-05 08:07:08 +0530

    update costsheet with scr

---
+++ meta.ts
@@ -0,0 +1,24 @@
+export const MAIN = {
+    PROJECT : 'SPECTRA',
+    SCR_EXCEL : 'SCR - ASBL Spectra as on 02-05-2023.xlsx',
+    PROJECTID : '6348a680f18f5f2282196152',
+}
+
+export const SPRINGS = {
+    PROJECT : 'SPRINGS',
+    SCR_EXCEL : 'SCR - ASBL Springs as on 04-05-2023.xlsx',
+    PROJECTID : '6348a680f18f5f2282196150',
+}
+
+export const SPECTRA = {
+    PROJECT : 'SPECTRA',
+    SCR_EXCEL : 'SCR - ASBL Spectra as on 02-05-2023.xlsx',
+    PROJECTID : '6348a680f18f5f2282196152',
+}
+
+export const SPIRE = {
+    PROJECT : 'SPIRE',
+    SCR_EXCEL : 'SCR - ASBL Spire as on 04-05-2023.xlsx',
+    PROJECTID : '6348a680f18f5f228219614e',
+}
+
